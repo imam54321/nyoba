50cb24d3cb617f49f299f420fc0e251a1ddeffd3
imam54321 <fauzanimam955gmail.com>
2026-07-12 14:35:09 +0700

    apaseehh

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,21 @@
-import serverless from "serverless-http";
-import app from "../src/types/app";
+import express from "express";
+import cors from "cors";
 
-export default serverless(app);
+import wargaRoutes from "../src/routes/wargaRoute";
+import pendudukRoutes from "../src/routes/pendudukRoute";
+import apbdesRoutes from "../src/routes/apbdesRoute";
+import authRoute from "../src/routes/authRoute";
+import beritaRoutes from "../src/routes/beritaRoute";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/penduduk", pendudukRoutes);
+app.use("/warga", wargaRoutes);
+app.use("/apbdes", apbdesRoutes);
+app.use("/berita", beritaRoutes);
+app.use("/api/auth", authRoute);
+
+export default app;
--- dist/src/routes/wargaRoute.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const wargaControllers_1 = require("../controllers/wargaControllers");
-const router = (0, express_1.Router)();
-router.get("/", wargaControllers_1.getStatistikPenduduk);
-exports.default = router;
